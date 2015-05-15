567984a4d5a4000a4fc22a9ef72cec2d4e861620
Travis Cheng <cheng.travis@gmail.com>
2015-05-15 14:57:23 -0700

    started working on java from book

---
+++ Coding Practice/Java/CrackCoding/src/stringsnarrays/Driver.java
@@ -0,0 +1,19 @@
+package stringsnarrays;
+
+import java.util.Scanner;
+
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+//		Uniquechars.testUniques(input);
+		Reversestring.testReverse(input);
+        
+		input.close();
+	}
+
+}
